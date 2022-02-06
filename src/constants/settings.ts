81c4c19d529fb0a24335d74592b09ed299e8c237
kedar <kedar.mhaswade@gmail.com>
2022-02-06 18:46:16 +0530

    close #8; note that this is temporary!

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,2 +1,3 @@
 export const MAX_WORD_LENGTH = 3
 export const MAX_CHALLENGES = 6
+export const RANDOM_DATE = true
--- src/lib/words.ts
+++ src/lib/words.ts
@@ -1,5 +1,6 @@
 import { WORDS } from '../constants/wordlist'
 import { VALIDGUESSES } from '../constants/validGuesses'
+import {RANDOM_DATE} from "../constants/settings";
 
 export const isWordInWordList = (word: string) => {
   return (
@@ -12,17 +13,31 @@ export const isWinningWord = (word: string) => {
   return solution === word
 }
 
+const epoch = new Date('January 1, 2022 00:00:00');
+const epochMs = epoch.valueOf()
+
+const generateRandomPastDate = (): number => { // thanks to https://stackoverflow.com/a/60180035/437506
+  const fromTime = epoch.getTime();
+  const toTime = new Date().getTime();
+  return new Date(fromTime + Math.random() * (toTime - fromTime)).valueOf();
+}
+
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
-  const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
   const now = Date.now()
   const msInDay = 86400000
-  const index = Math.floor((now - epochMs) / msInDay)
-  const nextday = (index + 1) * msInDay + epochMs
+  const today = Math.floor((now - epochMs) / msInDay)
+  const nextday = (today + 1) * msInDay + epochMs
+  let picked: number
+  if (RANDOM_DATE) {
+    picked = generateRandomPastDate()
+  } else {
+    picked = today
+  }
 
   return {
-    solution: WORDS[index % WORDS.length].toUpperCase(),
-    solutionIndex: index,
+    solution: WORDS[picked % WORDS.length].toUpperCase(),
+    solutionIndex: picked,
     tomorrow: nextday,
   }
 }
